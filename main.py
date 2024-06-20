63d909a1f102e1c31ec152529721203db9c6657e
code807 <code807@protonmail.com>
2024-06-20 13:21:08 -0700

    add service file and main

---
+++ main.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from pprint import pprint
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from enum import Enum
+import time
+import os
+import sqlite3
+import aiosqlite
+import requests
+
+twitch_key = os.environ['TWITCH_API_KEY']
+
+class ResponseMessage(BaseModel):
+    response: str
+
+class TwitchMessage(BaseModel):
+    subscription: SubscriptionData
+
+class SubscriptionData(BaseModel):
+    id: str | None = None
+    status: str
+    type: str
+    version: str
+    cost: int
+    condition: BroadcasterUserID
+
+class BroadcasterUserID():
+    broadcaster_user_id: str
+
+async def checkquota() -> bool:
+    return True
+
+app = FastAPI()
+
+async def checkquota(method):
+    return True
+
+@app.post("/twitch-handler")
+@app.post("/twitch-handler/")
+async def handle_message(data: TwitchMessage)  -> ResponseMessage:
+    q = data.q
+    target = data.target.value.upper()
+    source = data.source.value.upper()
+    user = data.user
+    api = data.method
+    return "what a mess"
